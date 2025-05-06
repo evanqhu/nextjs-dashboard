bb52d7969e44d81c24647042f554f2b8aabe8223
evanqhu <qianghu98@163.com>
2025-05-06 11:14:51 +0800

    feat: chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 /** 根布局 */
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts'; // 字体
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts"; // 字体
+import { Metadata } from "next";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
